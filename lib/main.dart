e70d557ea04304f4f445555d312f2728d036cb77
edipbaranavci <brn.avc17@gmail.com>
2023-03-31 20:32:38 +0300

    add apk

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
-import 'package:crewin_flutter/core/init/navigation/router/router_service.dart';
-import 'package:crewin_flutter/core/init/theme/custom_theme.dart';
-import 'package:crewin_flutter/features/views/splash/splash_view.dart';
+import 'core/init/navigation/router/router_service.dart';
+import 'core/init/theme/custom_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'cubit/main_cubit.dart';
-import 'features/views/home/view/home_view.dart';
 import 'firebase_options.dart';
 
 Future<void> _init() async {
